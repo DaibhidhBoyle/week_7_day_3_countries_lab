a92da59fa1b25cf0c5b3ad60e3db38621dfc55e2
Mark Mackay <Mark_Mackay@yahoo.com>
2019-04-10 14:11:37 +0100

    MVP done!

--- src/views/country_info_view.js
+++ src/views/country_info_view.js
@@ -17,9 +17,13 @@ CountryInfoView.prototype.buildElement = function(type, text, cls) {
   return element;
 }
 CountryInfoView.prototype.render = function(country){
+  console.dir(country);
   const infoParagraph = this.buildElement('div', '', 'country-info-item');
   const countryName = this.buildElement('h2', country.name);
-  // const familyDescription = this.buildElement('p', family.description);
+  const countryRegion = this.buildElement('p', country.region);
+  const countryFlag = document.createElement('img');
+  countryFlag.classList = 'flag';
+  countryFlag.src = country.flag;
   // const instruments = family.instruments;
   // const instrumentsHeader = this.buildElement('h3','Instruments Include:');
   // const instrumentsList = document.createElement('ul');
@@ -29,6 +33,8 @@ CountryInfoView.prototype.render = function(country){
   // });
 
   infoParagraph.appendChild(countryName);
+  infoParagraph.appendChild(countryRegion);
+  infoParagraph.appendChild(countryFlag);
   // infoParagraph.appendChild(familyDescription);
   // infoParagraph.appendChild(instrumentsHeader);
   // infoParagraph.appendChild(instrumentsList);
